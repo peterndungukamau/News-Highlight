56545824ed9e1b0b2634b679e059ff2ec95ae4ab
peterndungukamau <peterndungukamau9@gmail.com>
2019-11-16 13:41:13 +0300

    add application title

--- app/views.py
+++ app/views.py
@@ -6,5 +6,15 @@ def index():
     '''
     view page functions that returns an index page and its data
     '''
+    
+    title = 'News Highlight application'
+    return render_template('index.html',title = title)
 
-    return render_template('index.html')
+
+@app.route('/news/<int:news_id>')
+def news(news_id):
+    '''
+     view page that returns news details and its data
+    '''
+
+    return render_template('news.html',id = news_id)
