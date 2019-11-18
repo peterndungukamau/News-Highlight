9badcf5dd75231b09782debf55fa2518e6bfb7a1
peterndungukamau <peterndungukamau9@gmail.com>
2019-11-18 17:58:19 +0300

    test new instances

---
+++ app/news_test.py
@@ -0,0 +1,22 @@
+import unittest
+from models import news
+News = news.News
+
+class NewsTest(unittest.TestCase):
+    '''
+    Test Class to News Class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+
+        self.new_News = News(12,'Bbc','Good news','https://image.tmdb.org/t/p/w500/khsjha27hbs,8-12-200,)
+
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_news,News))
+
+if __name__ == '__main__':
+    unittest.main()        
