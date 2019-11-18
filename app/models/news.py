13c158d1d9c3f408392e2f2850d21714f492175f
peterndungukamau <peterndungukamau9@gmail.com>
2019-11-18 17:52:49 +0300

    add python package in models

---
+++ app/models/news.py
@@ -0,0 +1,10 @@
+class News:
+    '''
+    News class to News objects
+    '''
+
+    def __init__(self,id,name,description):
+        self.id = id
+        self.name = name
+        self.description = description
+        
