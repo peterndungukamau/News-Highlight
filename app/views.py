aeeb8220b14d8441fd0e6376fe92b21e02bab60d
peterndungukamau <peterndungukamau9@gmail.com>
2019-11-18 17:59:37 +0300

    modify views.py

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_news,get_new
 
 @app.route('/')
 def index():
@@ -7,14 +8,20 @@ def index():
     view page functions that returns an index page and its data
     '''
     
+    latest_news = get_news('latest')
     title = 'News Highlight application'
-    return render_template('index.html',title = title)
+    return render_template('index.html',title = title,latest = latest_news)
 
 
-@app.route('/news/<int:news_id>')
-def news(news_id):
+@app.route('/new/<int:id>')
+def new(id):
     '''
      view page that returns news details and its data
     '''
 
-    return render_template('news.html',id = news_id)
+    new = get_new(id)
+    title = f'{new.title}'
+ 
+    return render_template('news.html',title = title,new = new)
+
+
