05050aa245160351e9acc2b28bd0296147ac60d5
peterndungukamau <peterndungukamau9@gmail.com>
2019-11-19 12:11:00 +0300

    deploy

--- app/main/error.py
+++ app/main/error.py
@@ -1,7 +1,7 @@
 from flask import render_template
-from . import main
+from app import app
 
-@main.app_errorhandler(404)
+@app.errorhandler(404)
 def four_Ow_four(error):
     '''
     Function to render the 404 error page
